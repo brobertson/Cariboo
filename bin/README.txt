5061d5b78e384333a50bf2bec0f155a55cfc90a6
Bruce Robertson <brobertson@mta.ca>
2012-07-23 11:47:09 -0300

    remove unneeded files, and improved dtd caching

---
+++ bin/README.txt
@@ -0,0 +1,1 @@
+You need to provide the proper address of this 'Cariboo' directory to the xml:base variable in bin/lib/xmlcatalog.xml
--- bin/tei_p4_to_epub_and_mobi_testing.sh
+++ bin/tei_p4_to_epub_and_mobi_testing.sh
@@ -4,6 +4,7 @@ TMPDIR="/tmp"
 TEI2EPUBDIR=$BINDIR/tei2epub
 #TEI2EPUBDIR=/media/sda3/Summer_Work_Tine/Summer_Work_Backup/Cariboo-Summer/tei2epub
 COVERIMAGE="/home/ubuntu/Cariboo/Covers/Cariboo-cover.jpg"
+XML_CATALOG_FILES="/home/ubuntu/Cariboo/bin/lib/xmlcatalog.xml"
 short=$(basename $1)
 filename=${short%.*}
 extension=${short##*.}
@@ -17,18 +18,18 @@ p5=$TMPDIR/${filename}_gr_unic_p5_3.xml
 epub=$TMPDIR/${filename}.epub
 echo "Generating unicode TEI..."
 #perl -pe 's/<body>/<body lang="grc">/' $1 > $grecified
-xsltproc  $BINDIR/grecify.xsl $1 > $grecified
+XML_CATALOG_FILES=$XML_CATALOG_FILES xsltproc  $BINDIR/grecify.xsl $1 > $grecified
 java -classpath $BINDIR/lib -jar $BINDIR/transcoder-1.1-SNAPSHOT.jar -s $grecified -o ${unicode} -se BetaCode -oe UnicodeC
 #$BINDIR/transcode_perseus_tei.sh $grecified $unicode
 echo "Converting from P4 TEI to P5..."
-xsltproc $BINDIR/p4top5.xsl $unicode > $p5
+XML_CATALOG_FILES=$XML_CATALOG_FILES xsltproc $BINDIR/p4top5.xsl $unicode > $p5
 echo "Creating EPUB Document $p5"
 $TEI2EPUBDIR/bin/tei2epub.py $p5 ${filename} 
 
 echo "Creating image for $1 from $2" 
 echo "Grabbing author and title from xml..."
-author=`xsltproc $BINDIR/author.xsl $1 | tail -1`; 
-title=`xsltproc $BINDIR/title.xsl $1 | tail -1`;
+XML_CATALOG_FILES=$XML_CATALOG_FILES author=`XML_CATALOG_FILES=$XML_CATALOG_FILES xsltproc $BINDIR/author.xsl $1 | tail -1`; 
+XML_CATALOG_FILES=$XML_CATALOG_FILES title=`XML_CATALOG_FILES=$XML_CATALOG_FILES xsltproc $BINDIR/title.xsl $1 | tail -1`;
 title=${title/Machine readable text/}  
 echo "Now making image..."
 convert "$COVERIMAGE" -font Helvetica-Bold -fill black -pointsize 30 -annotate +44+460 "$author" $TMPDIR/${filename}_cover_temp.jpg
@@ -39,9 +40,9 @@ rm $TMPDIR/${filename}_cover_temp.jpg
 echo "Uncompressing Epub for modification ..."
 $BINDIR/unepub-me ${filename}.epub
 echo "Processing content.opf..."
-xsltproc $BINDIR/modify_content.xsl ${filename}/OEBPS/content.opf >  $TMPDIR/${filename}_new_content.opf
+XML_CATALOG_FILES=$XML_CATALOG_FILES xsltproc $BINDIR/modify_content.xsl ${filename}/OEBPS/content.opf >  $TMPDIR/${filename}_new_content.opf
 echo "Creating flyleaf ..."
-xsltproc $BINDIR/flyleaf.xsl $1 > $TMPDIR/${filename}_chapter-01.html
+XML_CATALOG_FILES=$XML_CATALOG_FILES xsltproc $BINDIR/flyleaf.xsl $1 > $TMPDIR/${filename}_chapter-01.html
 echo "Putting new content in epub filestructure ..." 
 cp $TMPDIR/${filename}_new_content.opf ${filename}/OEBPS/content.opf
 cp $TMPDIR/${filename}_cover.jpg ${filename}/OEBPS/cover.jpg
